4bc13541eb45353f81d512a080c92a4d717b4999
2023sandoval <emmanuelsandoval3@gmail.com>
2023-12-12 15:06:35 -0300

    first mod
    ..

---
+++ main/configuraciones/prod.py
@@ -0,0 +1,16 @@
+from .settings import*
+
+DEBUG = False
+
+ALLOWED_HOSTS = []
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'maindb',
+#         'USER': 'root',
+#         'PASSWORD': 'sebastopol123',
+#         'HOST': 'localhost',
+#         'PORT': '3306',
+#     }
+# } Por las dudas comento
